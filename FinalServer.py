3634ce2ddafa3e6eead5aa6c883bf123699d236a
znomon <kaseyfkolyno@gmail.com>
2015-11-24 19:24:52 -0500

    fixed username checking

--- FinalServer.py
+++ FinalServer.py
@@ -7,19 +7,11 @@ UDP_PORT = 1050
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((UDP_IP, UDP_PORT))
-
-def userCheck(name):
-    with open('username.txt', 'a+') as file:
-        if sys.getsizeof(file.readline()) == 21:
-            file.seek(0)
-            print >>file, name,"\n"
-        for line in file:
-            return 1
-            
-            
+sock.bind((UDP_IP, UDP_PORT))           
 
 def main():
+    userList = list()
+
     while True:
         data, addr = sock.recvfrom(512) # buffer size is 512 bytes
         frameArray = data.split(",")
@@ -29,9 +21,11 @@ def main():
             print "fA[1]: ", frameArray[1]
             username = username[:-1]
             print "username: ", username
-            if userCheck(username):
+            if username in userList:
                 print "username exists"
+                sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
             else:
+                userList.append(username)
                 print "username added"
         elif frameArray[0] == "URG":
             print "Unregister"
