3dfc2ebe92bf66d9b94733ef87edfb2d206b9e53
Znomon <kaseyfkolyno@gmail.com>
2015-11-29 22:56:14 -0500

    finished product?

--- FinalClient.py
+++ FinalClient.py
@@ -1,9 +1,14 @@
 import socket
+import time #Needed for sleep command
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "66.158.200.153"
 UDP_PORT = 1050
 #MESSAGE = "URG,znomon;"
 #MESSAGE = "QUO,znomon;"
+
+quoList = list() #List to remember stock values
+quoBool = None #Boolean value testing if user sends QUO or not
+servStock = list() #List to hold only the value of stocks
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
@@ -14,8 +19,31 @@ while 1:
 
     sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
-    data, addr = sock.recvfrom(512) # buffer size is 512 bytes
+    while 1: #Try to resend packet every 5 seconds if it gets lost
+        try: 
+            sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+            print "packet sent"
+            data, addr = sock.recvfrom(512) # buffer size is 512 bytes
+            if data: #If successfully sends break from loop
+                break
+            time.sleep(5) #Wait five seconds
+        except ValueError:
+            print "Resending Packet..."
 
-    print data
+    print data;
+    
+    if "QUO" in MESSAGE: #Store the stocks user asks for in a list
+        MESSAGE = MESSAGE[:-1] #Remove ; at end of string
+        quoList = MESSAGE.split(",")
+        del quoList[0] #Delete QUO 
+        del quoList[0] #Delete the username
+        quoBool = True
+
+    if quoBool: #Only do this when user sends QUO command
+        data = data[:-1] #Remove ; at end of string
+        servStock = data.split(",") #Break up string by delimeter
+        if servStock[0] == "ROK":
+            del servStock[0] #Remove the ROK to only get values
+            for i in range(len(servStock)):
+                 print quoList[i], servStock[i] #Prints prices for debugging
--- FinalServer.py
+++ FinalServer.py
@@ -16,16 +16,41 @@ def main():
     while True:
         data, addr = sock.recvfrom(512) 				# buffer size is 512 bytes
         frameArray = data.split(",")					#break up string on delimiter
-        username =  frameArray[1]					#username is equal to the second delimited string
-        username = username[:-1]					#username still includes a semicolon, strip that away
-        username = username.upper()                                     #stores all usernames as uppercase because usernames are not case sensitive
-        if not(username.isalnum()):                                     #if username is alpha-numeric
-            print "Invalid Username sent"
-            sock.sendto("INU;", addr)                                   #else send INvalid Username
 
         if frameArray[0] not in commands:                               #if command is not in the list of acceptable commands
         	sock.sendto("INC;", addr)                               #send INvalid Command
+        	continue
 
+        if frameArray[len(frameArray)-1].endswith(';'):                 #test to make sure request from client ends in a semicolon
+            if len(frameArray) < 2:
+                sock.sendto("INP;", addr)
+                continue
+            elif len(frameArray) == 2:
+                if frameArray[0] == "REG" or frameArray[0] == "UNR":
+                    username = frameArray[1]				#username is equal to the second delimited string
+                    username = username[:-1]				#username still includes a semicolon, strip that away
+                else:
+                    sock.sendto("INP;", addr)
+                    continue
+                print "FrameArray == 2"
+            elif len(frameArray) >= 3:
+                if frameArray[0] == "REG" or frameArray[0] == "UNR":
+                    sock.sendto("INP;",addr)
+                    continue
+                username = frameArray[1]                                #if this request contains stocks, set the second field to the username
+                print "FrameArray == 3"
+        else:
+            print "Forgot semicolon"                
+            sock.sendto("INP;", addr)                                   #if request from client doesn't end in a semicolon send INP
+            continue
+
+        username = username.upper()                                     #stores all usernames as uppercase because usernames are not case sensitive
+        if not(username.isalnum()) or len(username) > 32:               #if username is alpha-numeric
+            print "Invalid Username sent"
+            print "username length:", len(username)
+            sock.sendto("INU;", addr)                                   #else send INvalid Username
+            continue
+		
         if frameArray[0] == "REG":					#if first delimited string is REGister
             print "Register"
             print "fA[1]: ", frameArray[1]
@@ -42,12 +67,36 @@ def main():
             if username in userList:					#if user exists
                 userList.remove(username)
                 sock.sendto("ROK;", addr)
+                continue
             else:
                 print "username doesnt exist, can't remove"
                 sock.sendto("UNR;", addr)				#and then send Request OK
+                continue
         elif frameArray[0] == "QUO":
-            print "Quotes"
-
+            if username not in userList:				#if username doesnt exist
+                sock.sendto("UNR;", addr)
+                continue
+            quotes = list()
+            stockFile  = list(list())
+            output = "ROK"
+            for i in frameArray[2:]:
+                if i.endswith(';'):
+                    i = i[:-1]
+                quotes.append(i)
+            with open('stockfile.txt') as file:
+                for index, line in enumerate(file):
+                    stockFile.append(line.split(" "))
+            for i in quotes:
+                push = ",-1"
+                for j in stockFile:
+                    print "are these the same? :",j[0],": --> :",i,":" #debug
+                    
+                    if j[0] == i:
+                        push = "," + j[1]
+                output += push
+            output += ";"
+            sock.sendto(output, addr)
+            continue
 
         #Send outBuffer using client code
             
